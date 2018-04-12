0fbfd2da851fcf57e855cc8f58bd1153ed61470e
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-12 15:41:07 +0200

    Initialized webscraper

--- scrapers/jobindex-scraper-0.0.1.js
+++ scrapers/jobindex-scraper-0.0.1.js
@@ -6,20 +6,51 @@ const SUBJECT_CATEGORIES_LEVEL = 'https://it.jobindex.dk/job/it/database';
 
 async function run() {
     // DOM Selectors:
-    const JOBLIST_SELECTOR = '#result_list_box > div > div.results.component--default';
+    const JOBLIST_SELECTOR = 'PaidJob';
+    const LIST = '#result_list_box > div';
 
     // Initialization:
     const browser = await puppeteer.launch({
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto(TARGET_WEBSITE);
+
+    await page.goto('https://it.jobindex.dk/jobsoegning/it/database/storkoebenhavn');
+
+    // List length on one page:
+    let listLength = await page.evaluate((selector) => {
+        return document.getElementsByClassName(selector).length;
+    }, JOBLIST_SELECTOR);
+
+    console.log(listLength);
 
 
-    // First Level:
-    await page.goto(SUBJECT_AREA_LEVEL);
+    // UNSTABLE
 
-    // Second Level:
-    await page.goto(SUBJECT_CATEGORIES_LEVEL);
+    let divs = await page.evaluate((selector) => {
+        return document.getElementsByClassName(selector);
+    }, JOBLIST_SELECTOR);
+
+    let divChildren = divs.children;
+    for(let i = 0; i < divChildren.length; i++) {
+        let child = divChildren[i];
+        console.log(child);
+
+    }
+
+    console.log(divs);
+
+
+//    await page.goto(TARGET_WEBSITE);
+
+    /*
+        // First Level:
+        await page.goto(SUBJECT_AREA_LEVEL);
+
+        // Second Level:
+        await page.goto(SUBJECT_CATEGORIES_LEVEL);*/
+
+    // let a = '#result_list_box > div > div.results.component--default'
 }
+
 run();
