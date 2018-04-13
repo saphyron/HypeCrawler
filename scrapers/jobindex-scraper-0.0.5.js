9fd26e50f3c1094919a7c42b3c1a9cc6970cbbbe
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-13 16:34:55 +0200

    Update

--- scrapers/jobindex-scraper-0.0.5.js
+++ scrapers/jobindex-scraper-0.0.5.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
-
+const annonceModel = require('../model/annonce');
 const TARGET_WEBSITE = 'https://www.jobindex.dk';
 const SUBJECT_AREA_LEVEL = `https://www.jobindex.dk/job/it`;
 const SUBJECT_CATEGORIES_LEVEL = 'https://it.jobindex.dk/job/it/database';
 
 // Generic XPath to linked website element location:
-const LIST_ITEM_GENERIC_XPATH = '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/@href';
+const LIST_ITEM_URL_XPATH = '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/@href';
+const LIST_ITEM_TITLE_XPATH = '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a[2]/b';
 
 
 async function run() {
@@ -27,41 +28,48 @@ async function run() {
     }, JOBLIST_SELECTOR);
 
 
-    //await scrapePage(page, listLength);
+    await scrapePage(page, listLength);
 
 
-    // goto next page:
+/*    // goto next page:
     const GENERIC_PAGE_SELECTOR = 'https://it.jobindex.dk/jobsoegning/it/database/storkoebenhavn?page=PAGE_INDEX';
     const NUM_PAGES = await getNumPages(page, listLength);
 
-    for(let index = 2; index <= NUM_PAGES; index++) {
+    for (let index = 2; index <= NUM_PAGES; index++) {
         const PAGE_SELECTOR = GENERIC_PAGE_SELECTOR.replace('PAGE_INDEX', index);
 
         await page.goto(PAGE_SELECTOR);
-    }
+    }*/
 
 
     // Clean up:
-    // browser.close();
+    browser.close();
 
 }
 
 
 async function scrapePage(page, listLength) {
     // Iterate through a single page list and get linked sites for each advertisement:
-    for(let index = 1; index <= listLength; index++) {
-        const LIST_ITEM_SELECTOR = LIST_ITEM_GENERIC_XPATH.replace("INDEX", index);
+    for (let index = 1; index <= listLength; index++) {
 
+        // Extracting url
+        const LIST_ITEM_SELECTOR = LIST_ITEM_URL_XPATH.replace("INDEX", index);
         const LIST_ITEM_URL = await page.$x(LIST_ITEM_SELECTOR);
-        let itemUrl = await page.evaluate(div => div.textContent, LIST_ITEM_URL[0]);
-        console.log(itemUrl);
+        let itemUrl = await page.evaluate(div => div.textContent, LIST_ITEM_URL[1]);
 
-        /*        // Goto linked site and scrape it:
-                await page.goto(itemUrl);
 
-                const LINKED_SITE_BODY = await page.$x('/html/body');
-                let items = await page.evaluate(div => div.textContent, LINKED_SITE_BODY[0]);
-                console.log(items);*/
+        // Extracting title for advertisement:
+        const LIST_ITEM_TITLE_SELECTOR = LIST_ITEM_TITLE_XPATH.replace('INDEX', index);
+        const LIST_ITEM_TITLE = await page.$x(LIST_ITEM_TITLE_SELECTOR);
+        let annonceTitle = await page.evaluate(div => div.textContent, LIST_ITEM_TITLE[0]);
+
+
+        // Goto linked site and scrape it:
+        await page.goto(itemUrl);
+
+        const LINKED_SITE_BODY = await page.$x('/html/body');
+        let rawBodyText = await page.evaluate(div => div.textContent, LINKED_SITE_BODY[0]);
+        console.log(rawBodyText);
     }
 }
 
