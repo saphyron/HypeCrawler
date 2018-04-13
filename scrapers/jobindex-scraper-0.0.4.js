091fc4dd93a9eaa783120082353832180569b800
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-13 14:57:08 +0200

    Fetches websites and iterates through pages

---
+++ scrapers/jobindex-scraper-0.0.4.js
@@ -0,0 +1,77 @@
+const puppeteer = require('puppeteer');
+
+const TARGET_WEBSITE = 'https://www.jobindex.dk';
+const SUBJECT_AREA_LEVEL = `https://www.jobindex.dk/job/it`;
+const SUBJECT_CATEGORIES_LEVEL = 'https://it.jobindex.dk/job/it/database';
+
+// Generic XPath to linked website element location:
+const LIST_ITEM_GENERIC_XPATH = '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/@href';
+
+
+async function run() {
+    // DOM Selectors:
+    const JOBLIST_SELECTOR = 'PaidJob';
+
+    // Initialization:
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    await page.goto('https://it.jobindex.dk/jobsoegning/it/database/storkoebenhavn');
+
+
+    // List length on single page:
+    let listLength = await page.evaluate((selector) => {
+        return document.getElementsByClassName(selector).length;
+    }, JOBLIST_SELECTOR);
+
+
+    //await scrapePage(page, listLength);
+
+
+    // goto next page:
+    const GENERIC_PAGE_SELECTOR = 'https://it.jobindex.dk/jobsoegning/it/database/storkoebenhavn?page=PAGE_INDEX';
+    const NUM_PAGES = getNumPages(page, listLength);
+
+  /*  for(let index = 2; index <= 10; index++) {
+        const PAGE_SELECTOR = GENERIC_PAGE_SELECTOR.replace('PAGE_INDEX', index);
+
+        await page.goto(PAGE_SELECTOR);
+    }*/
+
+
+    // Clean up:
+    // browser.close();
+
+}
+
+
+async function scrapePage(page, listLength) {
+    // Iterate through a single page list and get linked sites for each advertisement:
+    for(let index = 1; index <= listLength; index++) {
+        const LIST_ITEM_SELECTOR = LIST_ITEM_GENERIC_XPATH.replace("INDEX", index);
+
+        const LIST_ITEM_URL = await page.$x(LIST_ITEM_SELECTOR);
+        let itemUrl = await page.evaluate(div => div.textContent, LIST_ITEM_URL[0]);
+        console.log(itemUrl);
+
+        /*        // Goto linked site and scrape it:
+                await page.goto(itemUrl);
+
+                const LINKED_SITE_BODY = await page.$x('/html/body');
+                let items = await page.evaluate(div => div.textContent, LINKED_SITE_BODY[0]);
+                console.log(items);*/
+    }
+}
+
+async function getNumPages(page, listLength) {
+    const TOTAL_ADVERTS_SELECTOR = '//*[@id="result_list_box"]/div/div[1]/div/div[1]/h2';
+    const ADVERTS_PER_PAGE = listLength;
+
+    let advertAmount = await page.evaluate(div => div.textContent, TOTAL_ADVERTS_SELECTOR[0]);
+    console.log(advertAmount);
+
+}
+
+run();
