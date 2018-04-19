fe965b048790e5f8f22700df46f93ad34236a7df
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-19 16:04:00 +0200

    New version of scraper method

--- scrapers/jobindex-scraper-0.0.11.js
+++ scrapers/jobindex-scraper-0.0.11.js
@@ -11,34 +11,44 @@ const SUBJECT_CATEGORIES_LEVEL = 'https://it.jobindex.dk/job/it/database';
 
 // Generic XPath to linked website element location:
 const LIST_ITEM_URL_XPATH = '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/@href';
-const LIST_ITEM_TITLE_XPATH =
-    '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/b or //*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a'; // TODO! Skal have implementeret en OR i XPATH.
+const LIST_ITEM_TITLE_XPATH = '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/*[1]';
+const LIST_ITEM_TITLE_XPATH_TWO =
+    '//*[@id="result_list_box"]/div/div[2]/div[INDEX]/div/a/strong'; // TODO! Jobindex increments index by TWO!
 
 // Counters:
-let succesCounter = 0, existingCounter = 0, errorCounter = 0;
+let successCounter = 0, existingCounter = 0, errorCounter = 0;
 
 // Variabels:
-let areaNames = ['storkoebenhavn', 'region-sjaelland', 'fyn', 'region-nordjylland', 'fyn', 'region-midtjylland',
-                    'sydjylland', 'bornholm', 'skaane', 'groenland', ''];
+const ADVERTS_PER_PAGE = 20;
+const AREA_NAMES = ['storkoebenhavn', 'region-sjaelland', 'fyn', 'region-nordjylland', 'fyn', 'region-midtjylland',
+    'sydjylland', 'bornholm', 'skaane', 'groenland', ''];
+const PATH_VARIATIONS = [
+    {
+        URL_XPATH_CLASS: 'PaidJob', URL_XPATH_ATTRIBUTES: '/a/@href', TITLE_XPATH_CLASS: 'PaidJob',
+        TITLE_ATTRIBUTES: '/a/*[1]'
+    },
+    {
+        URL_XPATH_CLASS: 'jix_robotjob', URL_XPATH_ATTRIBUTES: '/a/@href', TITLE_XPATH_CLASS: 'jix_robotjob',
+        TITLE_ATTRIBUTES: '/a/strong'
+    }
+];
 
 async function main() {
     // DOM Selectors:
     const JOBLIST_SELECTOR = 'PaidJob';
     // Initialization:
     const browser = await puppeteer.launch({
-        headless: false
+        headless: true
     });
     const page = await browser.newPage();
     await ORM.CreateAnnonceTable();
 
-    await page.goto('https://it.jobindex.dk/jobsoegning/it/storkoebenhavn');
-
+    await page.goto('https://it.jobindex.dk/jobsoegning/it/storkoebenhavn?page=26');
 
     // List length on single page: Put in scrapelist
     let listLength = await page.evaluate((selector) => {
         return document.getElementsByClassName(selector).length;
     }, JOBLIST_SELECTOR);
-
 
     await scrapeCategory(page, listLength);
     //let res = await scrapePageList(page, listLength);
@@ -51,23 +61,94 @@ async function main() {
 
 }
 
-async function scrapeCategory(page, listLength) {
+async function scrapeCategory(page) {
     // goto next page:
     const GENERIC_PAGE_SELECTOR = 'https://it.jobindex.dk/jobsoegning/it/storkoebenhavn?page=PAGE_INDEX';
-    const NUM_PAGES = await getNumPages(page, listLength);
+    const NUM_PAGES = await getNumPages(page, ADVERTS_PER_PAGE);
 
-    for (let index = 22; index <= NUM_PAGES; index++) {
+    for (let index = 1; index <= NUM_PAGES; index++) {
         console.log('BEGINNING SCRAPING ON PAGE: ' + index);
 
         const PAGE_SELECTOR = GENERIC_PAGE_SELECTOR.replace('PAGE_INDEX', index);
 
-        await page.goto(PAGE_SELECTOR);
-        await scrapePageList(page, listLength, PAGE_SELECTOR);
+        await scrapePageListV2(page, PAGE_SELECTOR);
 
     }
 
 }
 
+//<editor-fold desc="Test for improvement pagescraper">
+// Returnerer en liste med alle overskrifter:
+async function getPageTitles(page, listClassName, pathAfterClass) {
+
+    let xpathData = await page.$x(`//div[@class="${listClassName}"]${pathAfterClass}`);
+    let resList = [];
+    for (let i = 0; i < xpathData.length; i++) {
+        let xpathTextContent = await xpathData[i].getProperty('textContent');
+        let text = await xpathTextContent.jsonValue();
+        resList.push(text);
+    }
+
+    return resList;
+}
+
+async function getPageUrls(page, listClassName, pathAfterClass) {
+    let xpathData = await page.$x(`//div[@class="${listClassName}"]${pathAfterClass}`);
+    let resList = [];
+    for (let i = 0; i < xpathData.length; i++) {
+        let xpathTextContent = await xpathData[i].getProperty('textContent');
+        let text = await xpathTextContent.jsonValue();
+        resList.push(text);
+    }
+
+    return resList;
+}
+
+async function scrapePageListV2(page, PAGE_SELECTOR) {
+
+    await page.goto(PAGE_SELECTOR);
+
+    // TODO FLYT IND I METODE:
+    let counter = 0;
+    let titles = [], urls = [];
+
+    while (titles.length === 0 && counter < PATH_VARIATIONS.length) {
+        let currentObject = PATH_VARIATIONS[counter];
+        titles = await getPageTitles(page, currentObject.TITLE_XPATH_CLASS, currentObject.TITLE_ATTRIBUTES);
+        urls = await getPageUrls(page, currentObject.URL_XPATH_CLASS, currentObject.URL_XPATH_ATTRIBUTES);
+        counter++;
+    }
+    // =======================================================================================================
+    for (let index = 0; index < titles.length; index++) {
+        try {
+            console.log('Run ' + (index + 1) + ': begun');
+            console.time('runTime');
+
+            let annonceTitle = titles[index];
+            let itemUrl = urls[index];
+
+            // Goto linked site and scrape it:
+            await page.goto(itemUrl, {
+                // timeout: 1000  -- For later reference
+            });
+
+            const LINKED_SITE_BODY = await page.$x('/html/body');
+            let rawBodyText = await page.evaluate(div => div.textContent, LINKED_SITE_BODY[0]);
+            //console.log(rawBodyText);
+
+            // Insert or update annonce to database:
+            await insertAnnonce(annonceTitle, rawBodyText, itemUrl);
+            console.timeEnd('runTime');
+
+        } catch (e) {
+            console.log("SOMETHING WENT WRONG");
+            errorCounter++;
+        }
+    }
+
+}
+
+//</editor-fold>
 
 async function scrapePageList(page, listLength, currentPageUrl) {
     // Iterate through a single page list and get linked sites for each advertisement:
@@ -118,13 +199,13 @@ async function scrapePageList(page, listLength, currentPageUrl) {
 //<editor-fold desc="HelperMethods">
 
 function printDatabaseResult() {
-    let totalEntries = succesCounter + existingCounter + errorCounter;
+    let totalEntries = successCounter + existingCounter + errorCounter;
 
     console.log("\x1b[0m", '----------------------------------------------------------');
     console.log('\t\t\tJOBINDEX.DK SCRAPER STATISTIK');
     console.log("\x1b[0m", '----------------------------------------------------------');
-    console.log("\x1b[32m" + '\t\t\t' + succesCounter + ' OUT OF ' + totalEntries
-        + ` (${(succesCounter / totalEntries) * 100} %) --- INSERTS`);
+    console.log("\x1b[32m" + '\t\t\t' + successCounter + ' OUT OF ' + totalEntries
+        + ` (${(successCounter / totalEntries) * 100} %) --- INSERTS`);
     console.log("\x1b[0m", '----------------------------------------------------------');
 
     console.log("\x1b[33m" + '\t\t\t' + existingCounter + ' OUT OF ' + totalEntries
@@ -150,13 +231,14 @@ async function getNumPages(page, listLength) {
 }
 
 async function insertAnnonce(annonceTitle, rawBodyText, annonceURL) {
+    if (annonceTitle === '') return;
     let sha1Checksum = sha1(`${annonceTitle}${annonceURL}`);
     let callResult = await ORM.FindChecksum(sha1Checksum);
 
     if (callResult.length === 0) {
         let newAnnonceModel = await createAnnonceModel(annonceTitle, rawBodyText, null, sha1Checksum);
         await ORM.InsertAnnonce(newAnnonceModel);
-        succesCounter++;
+        successCounter++;
     }
     else {
         //console.log('ALREADY IN DATABASE!')// Do nothing - TODO create update method.
