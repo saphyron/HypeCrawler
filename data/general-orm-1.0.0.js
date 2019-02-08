f903a00b1081c8684313b773a8220b6b9dfc7630
mortenmathiasen <morten@tranebaerkaeret.dk>
2019-02-08 09:34:53 +0100

    Add PORT option to database connection

--- data/general-orm-1.0.0.js
+++ data/general-orm-1.0.0.js
@@ -6,6 +6,7 @@ const MYSQL = require('mysql');
 //<editor-fold desc="MySQL-connection">
 const DB_CONFIG = {
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
